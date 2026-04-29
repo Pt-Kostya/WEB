ef103216a8da6f59c9277a99b0d69edcca6b1c53
Konstantin <vvgg78343@gmail.com>
2026-04-29 18:56:13 +0200

    Com9

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, SelectField
-from wtforms.validators import Length, DataRequired 
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, PasswordField, BooleanField
+from wtforms.validators import Length, DataRequired, Email, EqualTo
 
 from .models import Category
 
@@ -14,3 +14,20 @@ class NewsForm(FlaskForm):
     text = TextAreaField('Описание новости', validators=[DataRequired(message='Поле не должно быть пустым')])
     category = SelectField('Категории', choices=get_categories())
     submit = SubmitField('Добавить')
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email(message='Некоректный email')])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField('Повторить пароль', validators=[DataRequired(), EqualTo('password', message='Пароли не совпадают')])
+    remember = BooleanField('Запомнить меня')
+    submit = SubmitField('Зарегистрироваться')
+
+
--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, url_for, redirect
+from flask_login import login_user, logout_user, login_required
 
 from . import app, db
-from .models import News, Category
-from .forms import NewsForm
+from .models import News, Category, User
+from .forms import NewsForm, RegistrationForm, LoginForm
 
 @app.route('/')
 def home_page():
@@ -17,6 +18,7 @@ def news_detail(id):
     return render_template('news_detail.html', news=news_d, categories=categories)
 
 @app.route('/add_news', methods= ['GET', 'POST'])
+@login_required
 def add_news():
     form = NewsForm()
     categories = Category.query.all()
@@ -37,3 +39,32 @@ def news_in_category(id):
     category_name = category.title
     categories = Category.query.all()
     return render_template('category.html', news=news, category_name=category_name, categories=categories)
+
+@app.route('/registration/', methods=['GET','POST'])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User()
+        user.username = form.username.data
+        user.name = form.name.data
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('registration.html', form=form)
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter(User.username == form.username.data).first()
+        if user and User.check_password(form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
