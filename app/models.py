9a4a4986ff66dcc093c47cf743016f5b7efee06b
Konstantin <vvgg78343@gmail.com>
2026-05-07 17:05:08 +0200

    com26

--- app/models.py
+++ app/models.py
@@ -44,3 +44,46 @@ class User(db.Model, UserMixin):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
